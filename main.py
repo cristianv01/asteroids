42a06135c51399db1808ae2debd52b865b3f57f8
cristianv01 <cristianv@vt.edu>
2025-05-20 21:46:57 -0400

    window set up

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+from constants import *
+
+def __main__():
+    print("Starting Asteroids!")
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+
+
+
+
+if __name__ == "__main__":
+    __main__()
